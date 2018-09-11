86aa8e2dee39f1722abdc07f61f6fa8f13115e62
yann.frizenschaf <yann.frizenschaf@ebor.com>
2018-09-11 16:11:19 +0930

    Allow up to 4 tracks with arbitrary names

--- GMapsFX-Examples/GPSTrackCompare/src/main/java/com/lynden/example/latlong/GpsTrackFXMLController.java
+++ GMapsFX-Examples/GPSTrackCompare/src/main/java/com/lynden/example/latlong/GpsTrackFXMLController.java
@@ -10,11 +10,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
 import com.lynden.gmapsfx.shapes.Polyline;
 import com.lynden.gmapsfx.shapes.PolylineOptions;
@@ -23,10 +20,24 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
-import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+/**
+ * Simple tool for comparing up to 4 GPS tracks from a text file with a particular format:
+ *
+ * File format:
+ * ----{TRACK NAME 1}----
+ * {lat, long, note, timeString}
+ * {lat, long, note, timeString}
+ * ...
+ * ----{TRACK NAME 2}----
+ * {lat, long, note, timeString}
+ * {lat, long, note, timeString}
+ * ...
+ *
+ *
+ */
 public class GpsTrackFXMLController implements Initializable {
 
     @FXML
@@ -39,33 +50,30 @@ public class GpsTrackFXMLController implements Initializable {
     private GoogleMapView googleMapView;
 
     @FXML
-    private CheckBox rawCheckbox;
+    private CheckBox set1Checkbox;
 
     @FXML
-    private CheckBox rejectedCheckbox;
+    private CheckBox set2Checkbox;
 
     @FXML
-    private CheckBox acceptedCheckbox;
+    private CheckBox set3Checkbox;
+
+    @FXML
+    private CheckBox set4Checkbox;
+
+    private List<CheckBox> checkBoxes = new ArrayList<>();
 
     private GoogleMap map;
 
     private DecimalFormat formatter = new DecimalFormat("###.00000");
 
-    private  List<LocationReason> raw = new ArrayList<>();
-    private List<LocationReason> accepted = new ArrayList<>();
-    private List<LocationReason> rejected = new ArrayList<>();
     private List<MapShape> lines = new ArrayList<>();
 
-    private boolean showRaw = true;
-    private boolean showAccepted = true;
-    private boolean showRejected = true;
-    private PointType type = null;
+    private Map<String, List<LocationReason>> pointSets;
 
-    private enum PointType {
-        RAW,
-        ACCEPTED,
-        REJECTED
-    }
+    private Map<String, Boolean> toggles = new HashMap<>();
+
+    private List<String> colours = new ArrayList<>();
 
     private class LocationReason {
         private LatLong location;
@@ -76,27 +84,28 @@ public class GpsTrackFXMLController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         googleMapView.addMapInitializedListener(() -> configureMap());
-        rawCheckbox.selectedProperty().addListener((observable, oldValue, newValue) ->
-        {
-            showRaw = newValue;
-            redraw();
+        checkBoxes.add(set1Checkbox);
+        checkBoxes.add(set2Checkbox);
+        checkBoxes.add(set3Checkbox);
+        checkBoxes.add(set4Checkbox);
+        checkBoxes.forEach(checkBox -> {
+            checkBox.setVisible(false);
+            checkBox.selectedProperty().addListener((observable, oldValue, newValue) ->
+            {
+                toggles.put(checkBox.getText(), newValue);
+                redraw();
+            });
         });
-        rejectedCheckbox.selectedProperty().addListener((observable, oldValue, newValue) ->
-        {
-            showRejected = newValue;
-            redraw();
-        });
-        acceptedCheckbox.selectedProperty().addListener((observable, oldValue, newValue) ->
-        {
-            showAccepted = newValue;
-            redraw();
-        });
+        colours.add("yellow");
+        colours.add("green");
+        colours.add("red");
+        colours.add("blue");
     }
 
     protected void configureMap() {
         MapOptions mapOptions = new MapOptions();
 
-        mapOptions.center(new LatLong(47.6097, -122.3331))
+        mapOptions.center(new LatLong(-34.927572, 138.597218))
                 .mapType(MapTypeIdEnum.ROADMAP)
                 .zoom(9);
         map = googleMapView.createMap(mapOptions, false);
@@ -124,24 +133,23 @@ public class GpsTrackFXMLController implements Initializable {
         }
     }
 
+    private String setName = null;
+
     private void importPoints(Path path) {
         try {
-            raw = new ArrayList<>();
-            accepted = new ArrayList<>();
-            rejected = new ArrayList<>();
+            setName = null;
+            pointSets = new HashMap<>();
             Files.lines(path).forEach(s -> {
+                Iterator<CheckBox> checkBoxIterator = checkBoxes.iterator();
                 if (s.startsWith("----")) {
-                    if (s.contains("UNFILTERED")) {
-                        type = PointType.RAW;
-                    }
-                    else if (s.contains("FILTERED")) {
-                        type = PointType.ACCEPTED;
-                    }
-                    else {
-                        type = PointType.REJECTED;
+                    setName = s.replaceAll("----","").trim();
+                    pointSets.put(setName, new ArrayList<>());
+                    toggles.put(setName, true);
+                    if (checkBoxIterator.hasNext()) {
+                        checkBoxIterator.next().setSelected(true);
                     }
                 }
-                else if (type != null) {
+                else if (setName != null) {
                     String[] split = s.split(";");
                     double lat = Double.parseDouble(split[0]);
                     double lon = Double.parseDouble(split[1]);
@@ -150,18 +158,7 @@ public class GpsTrackFXMLController implements Initializable {
                     locationReason.location = point;
                     locationReason.reason = split[2];
                     locationReason.time = split[3];
-                    switch (type) {
-                        case RAW:
-                            raw.add(locationReason);
-                            break;
-                        case ACCEPTED:
-                            accepted.add(locationReason);
-                            break;
-                        case REJECTED:
-                            rejected.add(locationReason);
-                            break;
-                    }
-
+                    pointSets.get(setName).add(locationReason);
                 }
             });
             redraw();
@@ -179,33 +176,45 @@ public class GpsTrackFXMLController implements Initializable {
             lines.forEach(line -> {
                 map.removeMapShape(line);
             });
-            if ((raw.isEmpty() && accepted.isEmpty() && rejected.isEmpty()) ||
-                    (!showRaw && !showAccepted && !showRejected)) {
+
+            if ((pointSets.isEmpty())) {
+                map.setCenter(new LatLong(-34.927572, 138.597218));
+                map.setZoom(9);
                 return;
             }
+
             LatLongBounds bounds = new LatLongBounds();
-            if (showRaw) {
-                drawRoute(raw, "yellow", bounds);
+            Iterator<CheckBox> checkBoxIterator = checkBoxes.iterator();
+            Iterator<String> colourIterator = colours.iterator();
+            for (String name : pointSets.keySet()) {
+                if (checkBoxIterator.hasNext()) {
+                    CheckBox checkBox =  checkBoxIterator.next();
+                    checkBox.setVisible(true);
+                    checkBox.setText(name);
+                }
+                boolean draw = toggles.get(name);
+                String colour = colourIterator.next();
+                if (draw) {
+                    drawRoute(pointSets.get(name), colour, bounds);
+                }
             }
-            if (showAccepted) {
-                drawRoute(accepted, "green", bounds);
+
+            while (checkBoxIterator.hasNext()) {
+                checkBoxIterator.next().setVisible(false);
             }
-            if (showRejected) {
-                drawRoute(rejected, "red", bounds);
-            }
+
             map.fitBounds(bounds);
         });
     }
 
     private InfoWindow openInfoWindow = null;
 
-    private LatLongBounds drawRoute(List<LocationReason> points, String color, LatLongBounds bounds) {
+    private void drawRoute(List<LocationReason> points, String color, LatLongBounds bounds) {
         LatLong prevPoint = null;
         for (final LocationReason point : points) {
             MarkerOptions markerOptions = new MarkerOptions();
             markerOptions.position(point.location)
                     .title(point.location.getLatitude() + ", " + point.location.getLongitude())
-//                    .label(point.reason)
                     .visible(true);
 
             final Marker marker = new Marker(markerOptions);
@@ -224,7 +233,6 @@ public class GpsTrackFXMLController implements Initializable {
             prevPoint = point.location;
             bounds.extend(point.location);
         }
-        return bounds;
     }
 
     private void drawLine(LatLong prevPoint, LatLong point, String color) {
